ace2b4a5ff5e137629c86683c0421a704b78eb0b
Loreto Dumitrescu <loretodalt@gmail.com>
2022-10-15 19:49:38 -0400

    simplify blocks

--- main.ts
+++ main.ts
@@ -1,27 +1,18 @@
+input.onPinPressed(TouchPin.P0, function () {
+    keyboard.sendString(keyboard.keys(keyboard._Key.tab))
+    basic.showArrow(ArrowNames.East)
+    basic.clearScreen()
+})
 bluetooth.onBluetoothConnected(function () {
     basic.showIcon(IconNames.Yes)
 })
 bluetooth.onBluetoothDisconnected(function () {
     basic.showIcon(IconNames.No)
 })
-keyboard.startKeyboardService()
-music.setBuiltInSpeakerEnabled(true)
-basic.showIcon(IconNames.Heart)
-basic.forever(function () {
-    if (input.pinIsPressed(TouchPin.P0)) {
-        keyboard.sendString(keyboard.keys(keyboard._Key.tab))
-        keyboard.releaseKeys()
-        basic.showArrow(ArrowNames.East)
-        music.playTone(262, music.beat(BeatFraction.Whole))
-        basic.clearScreen()
-        basic.pause(200)
-    }
-    if (input.pinIsPressed(TouchPin.P1)) {
-        keyboard.sendString(keyboard.keys(keyboard._Key.enter))
-        keyboard.releaseKeys()
-        basic.showArrow(ArrowNames.West)
-        music.playTone(330, music.beat(BeatFraction.Whole))
-        basic.clearScreen()
-        basic.pause(200)
-    }
+input.onPinPressed(TouchPin.P1, function () {
+    keyboard.sendString(keyboard.keys(keyboard._Key.enter))
+    basic.showIcon(IconNames.Yes)
+    basic.clearScreen()
 })
+keyboard.startKeyboardService()
+basic.showIcon(IconNames.Heart)
